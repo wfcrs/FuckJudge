b4755771ef2db9a48efb568a3b9bfcc1e6fc112d
btapple <bt_apple@qq.com>
2018-05-24 20:29:41 +0800

    init.

---
+++ FuckJudge.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         FuckJudge
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       btapple
+// @match        http://judge.buaa.edu.cn/assignment/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var a=document.querySelector("iframe[name='showmessage']");
+    var b=a.getAttribute("src");
+    var c=b.split("?")[1];
+    var d=document.querySelector(".tableline2").querySelectorAll("tr");
+    var input=document.createElement("input");
+    var e="http://judge.buaa.edu.cn/assignment/judgeDetailsRedirect.jsp?"+c;
+    input.setAttribute("onclick","window.location.href='"+e+"'");
+    input.setAttribute("value","烦死了")
+    input.setAttribute("type","submit")
+    d[1].querySelector("td").appendChild(input);
+})();
