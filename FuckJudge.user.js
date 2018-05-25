09479131f2b2d86c339c0502c98ac3448be3405d
yspjack <yspjack@users.noreply.github.com>
2018-05-26 01:28:33 +0800

    Only enable script in programList.jsp

--- FuckJudge.user.js
+++ FuckJudge.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       btapple
-// @match        http://judge.buaa.edu.cn/assignment/*
+// @match        http://judge.buaa.edu.cn/assignment/programList.jsp*
 // @grant        none
 // ==/UserScript==
 
