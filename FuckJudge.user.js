481a439dd0162e857871d6dbeb326353cc5b0854
Lynz Rand <plok314159265@163.com>
2018-06-07 12:42:22 +0800

    Add support for *.e.buaa.edu.cn/* vpn pages

--- FuckJudge.user.js
+++ FuckJudge.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         FuckJudge
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/wfcrs/FuckJudge
 // @version      0.1
 // @description  try to take over the world!
 // @author       btapple
-// @match        http://judge.buaa.edu.cn/assignment/programList.jsp*
+// @match        http://judge*.buaa.edu.cn/assignment/programList.jsp*
 // @grant        none
 // @supportURL   https://github.com/wfcrs/FuckJudge/issues
 // @updateURL    https://github.com/wfcrs/FuckJudge/raw/master/FuckJudge.user.js
