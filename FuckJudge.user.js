7ebc5a40b088ceca34c0f0bc089280020e255c34
Rynco Li <plok314159265@163.com>
2018-06-18 21:34:07 +0800

    Use hyperlink instead of button
    ... to allow viewing submit page without screwing things up.

--- FuckJudge.user.js
+++ FuckJudge.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         FuckJudge
 // @namespace    https://github.com/wfcrs/FuckJudge
-// @version      0.11
-// @description  try to take over the world!
-// @author       btapple
+// @version      0.13
+// @description  Screw judge.
+// @author       btapple && rynco
 // @match        *://judge.buaa.edu.cn/assignment/programList.jsp*
 // @match        *://judge.e.buaa.edu.cn/assignment/programList.jsp*
 // @grant        none
@@ -12,16 +12,24 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    var a=document.querySelector("iframe[name='showmessage']");
-    var b=a.getAttribute("src");
-    var c=b.split("?")[1];
-    var d=document.querySelector(".tableline2").querySelectorAll("tr");
-    var input=document.createElement("input");
+  function a() {
+    // var a = document.querySelector("iframe[name='showmessage']")
+    var b = a.getAttribute('src')
+    var c = b.split('?')[1]
+    // var d = a.contentDocument.querySelector(
+    //   'body > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > a:nth-child(1)'
+    // )
+    var input = document.createElement('a')
+    var submitButton = document.querySelector('#Submit');
     var websitePrefix = window.location.href.match(/\/\/j.*\.buaa\.edu\.cn/i)[0]
-    var e=websitePrefix+"/assignment/judgeDetailsRedirect.jsp?"+c;
-    input.setAttribute("onclick","window.location.href='"+e+"'");
-    input.setAttribute("value","烦死了")
-    input.setAttribute("type","submit")
-    d[1].querySelector("td").appendChild(input);
-})();
+    var e = websitePrefix + '/assignment/judgeDetailsRedirect.jsp?' + c
+    input.setAttribute('href', e)
+    input.innerHTML = '烦死了'
+    submitButton.after(input)
+    submitButton.after(' ')
+  }
+
+  setTimeout(a, 500)
+
+  // submitButton.onClick = ()=>{setTimeout(a, 1500)};
+})()
